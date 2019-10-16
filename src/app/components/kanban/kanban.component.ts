4d8c722f00c39f64fc9c0ef9cb98d33648d317c2
Maxim Baranov <maxim4220@gmail.com>
2019-10-16 18:01:15 +0300

    Added ngIf directive to template. Added method to switch buttons based on condition.

--- src/app/components/kanban/kanban.component.ts
+++ src/app/components/kanban/kanban.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kanban.component.scss']
 })
 export class KanbanComponent implements OnInit {
+  boardCreated = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createBoard() {
+    return this.boardCreated = this.boardCreated ? false : true;
   }
 
 }
