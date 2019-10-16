ea1b15733f83c6e2f01f9df7f5b3b7619df1c19f
Maxim Baranov <maxim4220@gmail.com>
2019-10-16 15:55:12 +0300

    Initial commit. Added kanban components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KanbanComponent } from './components/kanban/kanban.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KanbanComponent
   ],
   imports: [
     BrowserModule,
