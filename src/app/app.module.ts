c88c0793af5819a0955ffdecec607714babbb6c3
Maxim Baranov <maxim4220@gmail.com>
2019-10-16 17:58:32 +0300

    Added required imports to main module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KanbanComponent } from './components/kanban/kanban.component';
+import { BoardComponent } from './components/board/board.component';
+import { ListComponent } from './components/list/list.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    KanbanComponent
+    KanbanComponent,
+    BoardComponent,
+    ListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
