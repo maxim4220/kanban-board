a4ba4de4812c54b3d4d830b6def369e2c25c9c3b
Maxim Baranov <maxim4220@gmail.com>
2019-10-17 12:13:20 +0300

    Added logic to add fake users in app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserAuthService } from './services/user-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-board-app';
+  fakeUsers = [
+    {username: 'John Doe', password: 1111},
+    {username: 'John Smith', password: 2222},
+    {username: 'Alex Alex', password: 3333},
+    {username: 'Tom Smith', password: 4444},
+    {username: 'Andrew Thompson', password: 5555},
+    {username: 'Sam Anderson', password: 6666},
+  ]
+
+  constructor(userAuthService: UserAuthService) {
+    // If there is some data in local storage - continue.
+    if (userAuthService.getRegisteredUsersFromStorage()) {
+      console.log('if!!!!!!');
+     } else {
+         // If there are no users in local storage - add some fake users that will be used later in kanban logic.
+         userAuthService.addFakeUsers(this.fakeUsers);
+         console.log('fakeUsers', this.fakeUsers);
+     }
+  }
 }
