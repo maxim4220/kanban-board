8ca1ea1647d5e9fa9cdfba5f1f7231332412dcdb
Maxim Baranov <maxim4220@gmail.com>
2019-10-17 10:44:44 +0300

    Added logic for card component.

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Card} from '../../models/card-model';
 
 @Component({
-  selector: 'app-card',
+  selector: 'app-card-summary',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class SummaryComponent implements OnInit {
+
+  @Input() card: Card;
+  @Input() listIndex: number;
+  @Input() cardIndex: number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  identifyCardBeingDragged(dragEvent: DragEvent) {
+    console.log('dragEvent', dragEvent);
+    dragEvent.dataTransfer.effectAllowed = 'move'
+    dragEvent.dataTransfer.dropEffect= 'move'
+    const transferObject = {
+      'listIndex' : this.listIndex,
+      'cardIndex' : this.cardIndex
+    };
+    dragEvent.dataTransfer.setData( 'text', JSON.stringify(transferObject));
+   // dragEvent.preventDefault();
+  }
+
+  allowCardDragToBeDropped(dragEvent: DragEvent) {
+    dragEvent.dataTransfer.dropEffect= 'move'
+    dragEvent.preventDefault();
+  }
 }
