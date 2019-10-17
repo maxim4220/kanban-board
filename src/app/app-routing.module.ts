c3d5e3da9cb0bb63d331a81b3bfad6c9cdb95150
Maxim Baranov <maxim4220@gmail.com>
2019-10-17 11:19:01 +0300

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { KanbanComponent } from './components/kanban/kanban.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'kanban-board', component: KanbanComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: 'kanban-board' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
