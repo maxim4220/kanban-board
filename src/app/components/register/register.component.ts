85bf6d12854360cb67f0a05cee601389405e853a
Maxim Baranov <maxim4220@gmail.com>
2019-10-17 12:11:10 +0300

    Added logic to register users.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ConfirmPasswordValidator} from './confirm-password.validator';
+import {UserAuthService} from '../../services/user-service.service';
 
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
-})
+@Component({templateUrl: 'register.component.html'})
 export class RegisterComponent implements OnInit {
+ registerForm: FormGroup;
+ loading = false;
+ submitted = false;
+ returnUrl: string;
+ error: string;
+ constructor(
+   private formBuilder: FormBuilder,
+   private route: ActivatedRoute,
+   private router: Router,
+   private userAuthService: UserAuthService
+ ) {
+   // redirect to home if already logged in
+   if (this.userAuthService.getRegisteredUsersFromStorage()) {
+    // this.router.navigate(['/']);
+   }
+ }
 
-  constructor() { }
+ ngOnInit() {
+   this.registerForm = this.formBuilder.group({
+     username: ['', Validators.required],
+     password: ['', Validators.required],
+     confirmPassword: ['', Validators.required]
+   }, {
+     validator: ConfirmPasswordValidator.MatchPassword
+   });
+   // get return url from route parameters or default to '/'
+   this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+ }
 
-  ngOnInit() {
-  }
+ // convenience getter for easy access to form fields
+ get f() {
+   return this.registerForm.controls;
+ }
 
+ onSubmit() {
+   this.submitted = true;
+   // stop here if form is invalid
+   if (this.registerForm.invalid) {
+     return;
+   }
+   this.loading = true;
+   console.log('user register!', this.f.username.value, this.f.password.value);
+   const user = Object.assign({username: this.f.username.value}, {password: this.f.password.value});
+   console.log('user', user);
+   this.userAuthService.register(user);
+   this.loading = false;
+   return this.router.navigate(['/login']);
+ }
 }
