ddf95c4ec290810f9a83ad15d5a166a893085719
Maxim Baranov <maxim4220@gmail.com>
2019-10-18 11:21:34 +0300

    Added optional comment to card interface

--- src/app/models/card-model.ts
+++ src/app/models/card-model.ts
@@ -6,12 +6,13 @@ export interface CardInterface {
   assignedTo: string;
   color: string;
   isCompleted: boolean;
+  comments?: any;
 }
 
 export class Card implements CardInterface {
 
   constructor(public id: string, public header: string, public summary: string, public createdBy: string,
-              public assignedTo: string, public color: string, public isCompleted: boolean) {
+              public assignedTo: string, public color: string, public isCompleted: boolean, public comments?: any) {
   }
 
 }
