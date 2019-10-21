7b0631129a8d33cf435c92b7e4605f96abe5dde5
Maxim Baranov <maxim4220@gmail.com>
2019-10-21 09:50:06 +0300

    Added test to app component that tests DependencyInjection

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,12 @@
-import {TestBed, async} from '@angular/core/testing';
+import {TestBed, async, inject} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
+import { UserAuthService } from './services/user-service.service';
+//import { inject } from '@angular/core';
 
 describe('AppComponent', () => {
+  let testBedService: UserAuthService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -11,14 +15,23 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [UserAuthService]
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+   
     expect(app).toBeTruthy();
   });
+
+  it('Service injected via inject(...) and TestBed.get(...) should be the same instance',
+    inject([UserAuthService], (injectService: UserAuthService) => {
+    testBedService = TestBed.get(UserAuthService);
+      expect(injectService).toBe(testBedService);
+  })
+);
 
   it(`should have as title 'kanban-board-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
