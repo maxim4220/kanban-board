398454b25acc22c58357f5e9762dcb2abb853c0a
Maxim Baranov <maxim4220@gmail.com>
2019-10-21 10:40:43 +0300

    Added few test to check user service methods.

--- src/app/services/service.spec.ts
+++ src/app/services/service.spec.ts
@@ -1,4 +1,4 @@
-import {TestBed, async, inject, } from '@angular/core/testing';
+import {TestBed, async, inject, tick, } from '@angular/core/testing';
 import {UserAuthService} from './user-service.service';
 import {Router} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
@@ -17,6 +17,30 @@ describe('AppComponent', () => {
   };
 
   beforeEach(async(() => {
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+
+    spyOn(localStorage, 'getItem')
+    .and.callFake(mockLocalStorage.getItem);
+  spyOn(localStorage, 'setItem')
+    .and.callFake(mockLocalStorage.setItem);
+  spyOn(localStorage, 'removeItem')
+    .and.callFake(mockLocalStorage.removeItem);
+  spyOn(localStorage, 'clear')
+    .and.callFake(mockLocalStorage.clear);
 
     TestBed.configureTestingModule({
       providers: [
@@ -32,6 +56,32 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+ 
+    it('should store the test user data correctly in localStorage', inject([UserAuthService], (service: UserAuthService) =>{
+        let testUser = {username: "Sam Simons", password: 1111};
+        localStorage.setItem('usersRegistered', JSON.stringify(testUser));
+        expect(JSON.parse(localStorage.getItem('usersRegistered'))).toEqual(testUser);
+    }));
+
+    it('should return null when getting registered users from storage', inject([UserAuthService], (service: UserAuthService) =>{
+       let res =  service.getRegisteredUsersFromStorage();
+       // must be null, since no users have been added
+       expect(res).toBe(null);
+  }));
+
+  it('should be equal to array of fake users if fake users are added', inject([UserAuthService], (service: UserAuthService) =>{
+   let fakeUsers = [
+    {username: 'John Doe', password: 1111},
+    {username: 'John Smith', password: 2222},
+    {username: 'Alex Alex', password: 3333},
+    {username: 'Tom Smith', password: 4444},
+    {username: 'Andrew Thompson', password: 5555},
+    {username: 'Sam Anderson', password: 6666},
+  ]; 
+  service.addFakeUsers(fakeUsers);
+  let res =  service.getRegisteredUsersFromStorage();
+  expect(res).toEqual(fakeUsers);
+  }));
 
   it('should create user serviceservice', inject([UserAuthService], (service: UserAuthService) => {
     expect(service).toBeTruthy();
@@ -46,8 +96,8 @@ describe('AppComponent', () => {
   }));
 
   it('should return true', inject([UserAuthService], (service: UserAuthService) => {
-    //  expect(service.logout()).toHaveBeenCalledWith('/login');
-    //  tick(500);
+      // expect(service.logout()).toHaveBeenCalledWith('/login');
+      // tick(500);
   }));
 
 });
