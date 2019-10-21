b6089333dd8cca233f1b382edfd64765602d3896
Maxim Baranov <maxim4220@gmail.com>
2019-10-21 15:19:22 +0300

    Added guards for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,31 @@ import {Routes, RouterModule} from '@angular/router';
 import {KanbanComponent} from './components/kanban/kanban.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import { AuthGuardService } from './guards/auth-guard.service';
+import { ErrorComponent } from './components/error/error.component';
+import { SingUpGuardService } from './guards/sing-up-guard.service';
 
 
 export const routes: Routes = [
-  {path: 'kanban-board', component: KanbanComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  // otherwise redirect to home
-  {path: '**', redirectTo: 'kanban-board'}
+  { path: 'kanban-board', 
+    canActivate: [AuthGuardService],
+    component: KanbanComponent
+  },
+  {path: 'login',
+   canActivate: [SingUpGuardService],
+   component: LoginComponent},
+  {path: 'register',
+   canActivate: [SingUpGuardService],
+   component: RegisterComponent},
+  {path: 'error/404', component: ErrorComponent},
+  // otherwise redirect to 404 page
+   {path: '**', redirectTo: 'kanban-board'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuardService]
 })
 export class AppRoutingModule {
 }
