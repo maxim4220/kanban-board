74c46d0291087e3f47c44a99dde4989458236cfd
Maxim Baranov <maxim4220@gmail.com>
2019-10-22 17:06:56 +0300

    Added route to graphQl component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {RegisterComponent} from './components/register/register.component';
 import {AuthGuardService} from './guards/auth-guard.service';
 import {ErrorComponent} from './components/error/error.component';
 import {SingUpGuardService} from './guards/sing-up-guard.service';
+import { GraphQlComponent } from './components/graph-ql/graph-ql.component';
 
 
 export const routes: Routes = [
@@ -23,6 +24,10 @@ export const routes: Routes = [
     path: 'register',
     canActivate: [SingUpGuardService],
     component: RegisterComponent
+  },
+  {
+    path: 'graphQl',
+    component: GraphQlComponent
   },
   {path: 'error/404', component: ErrorComponent},
   // otherwise redirect to kanban board
