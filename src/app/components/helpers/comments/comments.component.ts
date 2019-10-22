ec53e944bd649467a68d08ff23d475b08ba11dfc
Maxim Baranov <maxim4220@gmail.com>
2019-10-22 09:36:32 +0300

    Added component to manage comments.

---
+++ src/app/components/helpers/comments/comments.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import swal from 'sweetalert2';
+@Component({
+  selector: 'app-comments',
+ templateUrl: './comments.component.html',
+})
+export class CommentsComponent implements OnInit {
+  @Input() comment;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addNestedComment(comment) {  
+    (async () => {
+      const {value: text} = await swal.fire({
+        title: 'add Review to comment',
+        input: 'textarea',
+        inputPlaceholder: 'Type your review for this commnent here...',
+        inputAttributes: {
+          'aria-label': 'Type your comment here'
+        },
+        showCancelButton: true
+      });
+      if (text) {
+          comment.subComments.push({comment: text, subComments: []} );
+      }
+    })();
+  }
+}
