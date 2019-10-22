625ef73494f65d193dcadd537c276529717cd35c
Maxim Baranov <maxim4220@gmail.com>
2019-10-22 15:27:18 +0300

    Added graphQL dependencies including appoloClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import {RegisterComponent} from './components/register/register.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ErrorComponent} from './components/error/error.component';
 import {CommentsComponent} from './components/helpers/comments/comments.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @NgModule({
   declarations: [
@@ -33,10 +37,19 @@ import {CommentsComponent} from './components/helpers/comments/comments.componen
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({uri: '[URL]'}),
+      cache: new InMemoryCache()
+    });
+  }
 }
