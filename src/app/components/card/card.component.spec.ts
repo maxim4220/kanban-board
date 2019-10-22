e60d2247ebb2c4d0fb2a1b69780230900fed0580
Maxim Baranov <maxim4220@gmail.com>
2019-10-22 12:50:25 +0300

    Added few tests for a card component

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,5 +1,11 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SummaryComponent} from './card.component';
+import {ContentEditDirective} from '../../directives/content-edit.directive';
+import {Card} from '../../models/card-model';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ContextMenuComponent} from '../context/context-menu.component/context-menu.component';
+import {CommentsComponent} from '../helpers/comments/comments.component';
 
 describe('SummaryComponent', () => {
   let component: SummaryComponent;
@@ -7,15 +13,27 @@ describe('SummaryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SummaryComponent],
-      // providers: [UserAuthService,],
-      //  imports: [FormsModule, ReactiveFormsModule, RouterTestingModule,]
+      declarations: [SummaryComponent,
+        ContentEditDirective, ContextMenuComponent, CommentsComponent],
+      imports: [FormsModule, ReactiveFormsModule, RouterTestingModule]
     })
       .compileComponents();
   }));
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(SummaryComponent);
     component = fixture.componentInstance;
-  }));
+    component.card = new Card('1', 'Title', 'Description', 'This is a description', 'Tony', 'not assigned', false);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have header', () => {
+    expect(component).toBeTruthy();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Title');
+  });
 });
